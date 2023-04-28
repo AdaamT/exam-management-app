d812e02ac61da4bc33dc832f58169a2aed7519c6
Adam Tai <adamtai@hotmail.co.uk>
2023-04-28 14:47:15 +0100

    Examlist now showing, updated get request to localhost

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import ExamList from "./components/ExamList";
+import axios from "axios";
 
 function App() {
+  const [exams, setExams] = useState([]);
+
+  useEffect(() => {
+    // Fetch the exams data from the API
+    axios
+      .get("http://localhost:3000/api/exams")
+      .then((response) => {
+        console.log("Exams data:", response.data); // Log the exams data
+        setExams(response.data);
+      })
+      .catch((error) => {
+        console.log("Error fetching exams:", error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Exam List</h1>
+      <ExamList exams={exams} />
     </div>
   );
 }
