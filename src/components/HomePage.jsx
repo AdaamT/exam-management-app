625a8d7d4024394502c359808c92304379066a74
Adam Tai <adamtai@hotmail.co.uk>
2023-04-28 14:55:28 +0100

    Homepage componenet made

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+      <div>
+        <h1>Welcome to the Homepage</h1>
+        <button>Show All Exams</button>
+        {/* Add other buttons for different pages later */}
+      </div>
+    );
+  };
+  
+  export default HomePage;
+  
